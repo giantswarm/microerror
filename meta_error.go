3f294ee5495761b404de2b24247e0403e3e4bfd0
Tim Schindler <xh3b4sd@gmail.com>
2018-07-30 09:54:09 +0200

    renames MetaError to Error

--- meta_error.go
+++ meta_error.go
@@ -4,14 +4,14 @@ import (
 	"encoding/json"
 )
 
-// MetaError is a predefined error structure which purpose is to act as
-// container for meta information associated to a specific error. The specific
-// error type matching can be used as usual. The usual error masking and cause
-// gathering can be used as usual. Using MetaError might look as follows. In the
-// beginning is a usual error defined, along with its matcher. This error is the
-// root cause once emitted during runtime.
+// Error is a predefined error structure which purpose is to act as container
+// for meta information associated to a specific error. The specific error type
+// matching can be used as usual. The usual error masking and cause gathering
+// can be used as usual. Using Error might look as follows. In the beginning is
+// a usual error defined, along with its matcher. This error is the root cause
+// once emitted during runtime.
 //
-//     var notEnoughWorkersError = microerror.MetaError{
+//     var notEnoughWorkersError = microerror.Error{
 //         Desc: "The amount of requested guest cluster workers exceeds the available number of host cluster nodes.",
 //         Docs: "https://github.com/giantswarm/ops-recipes/blob/master/349-not-enough-workers.md",
 //         Kind: "notEnoughWorkersError",
@@ -21,22 +21,22 @@ import (
 //         return microerror.Cause(err) == notEnoughWorkersError
 //     }
 //
-type MetaError struct {
+type Error struct {
 	Desc string `json:"desc"`
 	Docs string `json:"docs"`
 	Kind string `json:"kind"`
 }
 
-func (m *MetaError) Error() string {
-	return toStringCase(m.Kind)
+func (e *Error) Error() string {
+	return toStringCase(e.Kind)
 }
 
-func (m *MetaError) GoString() string {
-	return m.String()
+func (e *Error) GoString() string {
+	return e.String()
 }
 
-func (m *MetaError) MarshalJSON() ([]byte, error) {
-	b, err := json.Marshal(m)
+func (e *Error) MarshalJSON() ([]byte, error) {
+	b, err := json.Marshal(e)
 	if err != nil {
 		return nil, Mask(err)
 	}
@@ -44,8 +44,8 @@ func (m *MetaError) MarshalJSON() ([]byte, error) {
 	return b, nil
 }
 
-func (m *MetaError) String() string {
-	b, err := m.MarshalJSON()
+func (e *Error) String() string {
+	b, err := e.MarshalJSON()
 	if err != nil {
 		panic(err)
 	}
@@ -53,14 +53,14 @@ func (m *MetaError) String() string {
 	return string(b)
 }
 
-func (m *MetaError) UnmarshalJSON(b []byte) error {
-	var c MetaError
+func (e *Error) UnmarshalJSON(b []byte) error {
+	var c Error
 	err := json.Unmarshal(b, &c)
 	if err != nil {
 		return Mask(err)
 	}
 
-	*m = c
+	*e = c
 
 	return nil
 }
