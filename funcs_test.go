b50d4dffbd2393286ead2d739462f276fa284f75
Paweł Kopiczko <kopiczko@gmail.com>
2019-06-05 11:17:23 +0100

    add Stack function
    It often happens that we print third party error which has a pointers in
    it then we can have useless output like:
    
    ```
    E 06/05 09:55:10 failed to update target stack `cluster-no6h7-guest-recordsets` | route53-manager/pkg/recordset/recordset.go:378 | stack=&awserr.requestError{awsError:(*awserr.baseError)(0xc00056c940), statusCode:400, requestID:"01bdc5cd-8778-11e9-a07f-ed66816531c5"}
    ```
    
    Using `"stack", microerror.Stack(err)` instead of
    `"stack", fmt.Sprintf("%#v", err)` in our logs should solve the issue.

---
+++ funcs_test.go
@@ -0,0 +1,41 @@
+package microerror
+
+import (
+	"errors"
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func Test_Stack(t *testing.T) {
+	testCases := []struct {
+		name          string
+		inputErr      error
+		expectedStack string
+	}{
+		{
+			name:          "case 0: annotated microerror error",
+			inputErr:      Maskf(&Error{Kind: "testKind"}, "annotation"),
+			expectedStack: "[{/Users/pawel/go/src/github.com/giantswarm/microerror/funcs_test.go:18: annotation} {test kind}]",
+		},
+		{
+			name:          "case 1: non annotated microerror error",
+			inputErr:      &Error{Kind: "testKind"},
+			expectedStack: "test kind",
+		},
+		{
+			name:          "case 2: external error",
+			inputErr:      errors.New("external error"),
+			expectedStack: "external error",
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			stack := Stack(tc.inputErr)
+			if !reflect.DeepEqual(stack, tc.expectedStack) {
+				t.Fatalf("stack = %q, want %q", stack, tc.expectedStack)
+			}
+		})
+	}
+}
