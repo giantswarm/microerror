2520b0f469f2fde656cc451fa2c811f9fa26370b
Paweł Kopiczko <kopiczko@gmail.com>
2019-06-05 15:56:26 +0100

    test on regexp

--- funcs_test.go
+++ funcs_test.go
@@ -2,40 +2,44 @@ package microerror
 
 import (
 	"errors"
-	"go/build"
-	"reflect"
+	"regexp"
 	"strconv"
 	"testing"
 )
 
 func Test_Stack(t *testing.T) {
 	testCases := []struct {
-		name          string
-		inputErr      error
-		expectedStack string
+		name                string
+		inputErr            error
+		expectedStackRegexp string
 	}{
 		{
-			name:          "case 0: annotated microerror error",
-			inputErr:      Maskf(&Error{Kind: "testKind"}, "annotation"),
-			expectedStack: "[{" + build.Default.GOPATH + "/src/github.com/giantswarm/microerror/funcs_test.go:19: annotation} {test kind}]",
+			name:                "case 0: annotated microerror error",
+			inputErr:            Maskf(&Error{Kind: "testKind"}, "annotation"),
+			expectedStackRegexp: `^\[\{[a-zA-Z_/-]*/src/github.com/giantswarm/microerror/funcs_test.go:\d+: annotation\} \{test kind\}\]$`,
 		},
 		{
-			name:          "case 1: non annotated microerror error",
-			inputErr:      &Error{Kind: "testKind"},
-			expectedStack: "test kind",
+			name:                "case 1: non annotated microerror error",
+			inputErr:            &Error{Kind: "testKind"},
+			expectedStackRegexp: "^test kind$",
 		},
 		{
-			name:          "case 2: external error",
-			inputErr:      errors.New("external error"),
-			expectedStack: "external error",
+			name:                "case 2: external error",
+			inputErr:            errors.New("external error"),
+			expectedStackRegexp: "^external error$",
 		},
 	}
 
 	for i, tc := range testCases {
+		re, err := regexp.Compile(tc.expectedStackRegexp)
+		if err != nil {
+			t.Fatalf("err = %q, want nil", Stack(err))
+		}
+
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			stack := Stack(tc.inputErr)
-			if !reflect.DeepEqual(stack, tc.expectedStack) {
-				t.Fatalf("stack = %q, want %q", stack, tc.expectedStack)
+			if !re.MatchString(stack) {
+				t.Fatalf("stack = %q, want matching regexp %#q", stack, tc.expectedStackRegexp)
 			}
 		})
 	}
