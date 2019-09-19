73b3452f396e82287ba42704ed7114af0220b4f6
Paweł Kopiczko <kopiczko@gmail.com>
2019-09-19 17:37:42 +0100

    microerror: use errors package

--- microerror.go
+++ microerror.go
@@ -1,43 +1,155 @@
-// Package microerror provides project wide helper functions for a more convenient
-// and efficient error handling.
 package microerror
 
-var (
-	handler Handler = NewErrgoHandler(ErrgoHandlerConfig{
-		CallDepth: 1,
-	})
+import (
+	"encoding/json"
+	"fmt"
+	"runtime"
+
+	"errors"
 )
 
-// New returns a new error with the given error message. It is a drop-in
-// replacement for errors.New from the standard library.
-func New(s string) error {
-	return handler.New(s)
+const kindUnknown = "unknown"
+
+type Error struct {
+	Desc string `json:"desc,omitempty"`
+	Docs string `json:"docs,omitempty"`
+	Kind string `json:"kind"`
 }
 
-// Newf returns a new error with the given printf-formatted error message.
-func Newf(f string, v ...interface{}) error {
-	return handler.Newf(f, v...)
+func (e Error) Error() string {
+	return toStringCase(e.Kind)
 }
 
-// Cause returns the cause of the given error. If the cause of the err can not
-// be found it returns the err itself.
-//
-// Cause is the usual way to diagnose errors that may have been wrapped by Mask
-// or Maskf.
-func Cause(err error) error {
-	return handler.Cause(err)
+type annotatedError struct {
+	annotation string
+	underlying Error
 }
 
-// Mask is a simple error masker. Masked errors act as tracers within the
-// source code. Inspecting an masked error shows where the error was passed
-// through within the code base. This is gold for debugging and bug hunting.
+func (e annotatedError) Error() string {
+	if e.annotation == "" {
+		return e.underlying.Error()
+	}
+	return e.underlying.Error() + ": " + e.annotation
+}
+
+func (e annotatedError) MarshalJSON() ([]byte, error) {
+	o := JSONError{
+		Error: e.underlying,
+
+		Annotation: e.annotation,
+	}
+
+	bytes, err := json.Marshal(o)
+	if err != nil {
+		return nil, fmt.Errorf("microerror.annotatedError.MarshalJSON: %w object=%#v", err, o)
+	}
+
+	return bytes, nil
+}
+
+func (e annotatedError) Unwrap() error {
+	return e.underlying
+}
+
+type stackedError struct {
+	stackEntry StackEntry
+	underlying error
+}
+
+func (e stackedError) Error() string {
+	return e.underlying.Error()
+}
+
+func (e stackedError) MarshalJSON() ([]byte, error) {
+	var stack = []StackEntry{
+		e.stackEntry,
+	}
+	{
+		underlying := e.underlying
+		var stacked stackedError
+		for errors.As(underlying, &stacked) {
+			stack = append([]StackEntry{stacked.stackEntry}, stack...)
+			underlying = stacked.underlying
+		}
+	}
+
+	var microErr Error
+	var annotation string
+	{
+		if errors.As(e, &microErr) {
+			var annotatedErr annotatedError
+			if errors.As(e, &annotatedErr) {
+				annotation = annotatedErr.annotation
+			}
+		} else {
+			microErr = Error{
+				Kind: kindUnknown,
+			}
+
+			annotation = e.Error()
+		}
+	}
+
+	o := JSONError{
+		Error: microErr,
+
+		Annotation: annotation,
+		Stack:      stack,
+	}
+
+	bytes, err := json.Marshal(o)
+	if err != nil {
+		return nil, fmt.Errorf("microerror.stackedError.MarshalJSON: %w object=%#v", err, o)
+	}
+
+	return bytes, nil
+}
+
+func (e stackedError) Unwrap() error {
+	return e.underlying
+}
+
+type JSONError struct {
+	Error `json:",inline"`
+
+	Annotation string       `json:"annotation,omitempty"`
+	Stack      []StackEntry `json:"stack,omitempty"`
+}
+
+type StackEntry struct {
+	File string `json:"file"`
+	Line int    `json:"line"`
+}
+
+func Is(err, target error) bool {
+	return errors.Is(err, target)
+}
+
+func Maskf(err Error, f string, v ...interface{}) error {
+	annotatedErr := annotatedError{
+		annotation: fmt.Sprintf(f, v...),
+		underlying: err,
+	}
+
+	return mask(annotatedErr)
+}
+
 func Mask(err error) error {
-	return handler.Mask(err)
+	if err == nil {
+		return nil
+	}
+
+	return mask(err)
 }
 
-// Maskf is like Mask. In addition to that it takes a format string and
-// variadic arguments like fmt.Sprintf. The format string and variadic
-// arguments are used to annotate the given errgo error.
-func Maskf(err error, f string, v ...interface{}) error {
-	return handler.Maskf(err, f, v...)
+func mask(err error) error {
+	_, file, line, _ := runtime.Caller(2)
+
+	return stackedError{
+		stackEntry: StackEntry{
+			File: file,
+			Line: line,
+		},
+		underlying: err,
+	}
 }
