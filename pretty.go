5d713f152236b4f24daaf0d223471a72884a4762
axbarsan <mnlk123@yahoo.com>
2020-07-22 20:51:29 +0200

    Add helper for pretty printing error message

---
+++ pretty.go
@@ -0,0 +1,93 @@
+package microerror
+
+import (
+	"errors"
+	"strings"
+	"unicode"
+)
+
+const (
+	prefix = "Error: "
+	suffix = " error"
+)
+
+func Pretty(err error) string {
+	var message string
+
+	// Check if it's an annotated error.
+	var aErr *annotatedError
+	isAnnotatedErr := errors.As(err, &aErr)
+	if isAnnotatedErr {
+		message = aErr.annotation
+	} else {
+		// This is either an unmasked microerror, or
+		// a simple 'errors.New()' error.
+		message = err.Error()
+	}
+
+	message = prettifyErrorMessage(message)
+
+	return message
+}
+
+func prettifyErrorMessage(message string) string {
+	if len(message) < 1 {
+		return message
+	}
+
+	r := []rune(message)
+	// Pre-assign with the "Error: " prefix.
+	m := []rune(prefix)
+
+	var (
+		nestedPos  int
+		nestedChar rune
+	)
+
+	for pos := 0; pos < len(r); pos++ {
+		if len(r) > len(prefix) && pos < len(prefix) {
+			for nestedPos, nestedChar = range strings.ToLower(prefix) {
+				if unicode.ToLower(r[pos+nestedPos]) != nestedChar {
+					break
+				}
+
+				// Ignore the 'error: ' prefix if it exists.
+				if nestedPos == len(prefix)-1 {
+					pos += len(prefix)
+					// Capitalize the first letter of the message.
+					m = append(m, unicode.ToUpper(r[pos]))
+					pos++
+				}
+			}
+		}
+
+		if len(r)+pos > len(suffix) && pos == len(r)-len(suffix) {
+			for nestedPos, nestedChar = range strings.ToLower(suffix) {
+				if unicode.ToLower(r[pos+nestedPos]) != nestedChar {
+					break
+				}
+
+				// Ignore the ' error' suffix if it exists.
+				// This suffix is usually present in microerrors
+				// without annotations.
+				if nestedPos == len(suffix)-1 {
+					pos += len(suffix)
+				}
+			}
+		}
+
+		// We're already done, let's exit.
+		if pos == len(r) {
+			break
+		}
+
+		char := r[pos]
+		// Capitalize the first letter of the message.
+		if pos == 0 {
+			char = unicode.ToUpper(char)
+		}
+		m = append(m, char)
+	}
+
+	return string(m)
+}
---
+++ pretty_test.go
@@ -0,0 +1,123 @@
+package microerror
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestPretty(t *testing.T) {
+	testCases := []struct {
+		name            string
+		errorFactory    func() error
+		expectedMessage string
+	}{
+		{
+			name: "case 0: simple error",
+			errorFactory: func() error {
+				err := errors.New("something went wrong")
+
+				return err
+			},
+			expectedMessage: "Error: Something went wrong",
+		},
+		{
+			name: "case 1: simple empty error",
+			errorFactory: func() error {
+				err := errors.New("")
+
+				return err
+			},
+			expectedMessage: "",
+		},
+		{
+			name: "case 2: simple error with 'error:' prefix in message",
+			errorFactory: func() error {
+				err := errors.New("error: something went wrong")
+
+				return err
+			},
+			expectedMessage: "Error: Something went wrong",
+		},
+		{
+			name: "case 3: masked simple error",
+			errorFactory: func() error {
+				err := errors.New("something went wrong")
+
+				return Mask(err)
+			},
+			expectedMessage: "Error: Something went wrong",
+		},
+		{
+			name: "case 4: microerror, 0 depth",
+			errorFactory: func() error {
+				err := &Error{
+					Kind: "somethingWentWrongError",
+				}
+
+				return err
+			},
+			expectedMessage: "Error: Something went wrong",
+		},
+		{
+			name: "case 5: microerror, 1 depth",
+			errorFactory: func() error {
+				err := &Error{
+					Kind: "somethingWentWrongError",
+				}
+
+				return Mask(err)
+			},
+			expectedMessage: "Error: Something went wrong",
+		},
+		{
+			name: "case 6: microerror, 1 depth, with annotation",
+			errorFactory: func() error {
+				err := &Error{
+					Kind: "somethingWentWrongError",
+				}
+
+				return Maskf(err, "something bad happened, and we had to crash")
+			},
+			expectedMessage: "Error: Something bad happened, and we had to crash",
+		},
+		{
+			name: "case 7: microerror, 1 depth, with multiline annotation",
+			errorFactory: func() error {
+				err := &Error{
+					Kind: "somethingWentWrongError",
+				}
+
+				return Maskf(err, "something bad happened, and we had to crash\nthat's the first time it happened, really")
+			},
+			expectedMessage: "Error: Something bad happened, and we had to crash\nthat's the first time it happened, really",
+		},
+		{
+			name: "case 8: microerror, 10 depth",
+			errorFactory: func() error {
+				err := &Error{
+					Kind: "somethingWentWrongError",
+				}
+
+				// Let's build up this stack trace.
+				newErr := Mask(err)
+				for i := 0; i < 11; i++ {
+					newErr = Mask(newErr)
+				}
+
+				return newErr
+			},
+			expectedMessage: "Error: Something went wrong",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.errorFactory()
+			message := Pretty(err)
+
+			if message != tc.expectedMessage {
+				t.Fatalf("expected %q got %q", tc.expectedMessage, message)
+			}
+		})
+	}
+}
