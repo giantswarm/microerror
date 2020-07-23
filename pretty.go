9b5e49f7e4f68bdfb94b068442609ad021a3a2b7
axbarsan <mnlk123@yahoo.com>
2020-07-23 13:01:07 +0200

    Refactor error message prettifying, so humans would be able to understand it

--- pretty.go
+++ pretty.go
@@ -35,63 +35,24 @@ func prettifyErrorMessage(message string) string {
 		return message
 	}
 
-	r := []rune(message)
-	// Pre-assign with the "Error: " prefix.
-	m := []rune(prefix)
+	// Remove the 'Error: ' prefix if it exists
+	if strings.HasPrefix(strings.ToLower(message), strings.ToLower(prefix)) {
+		message = message[len(prefix):]
+	}
+	// This suffix is usually present in microerrors
+	// without annotations.
+	message = strings.TrimSuffix(message, " error")
 
-	var (
-		nestedPos  int
-		nestedChar rune
-	)
-
-	for pos := 0; pos < len(r); pos++ {
-		if len(r) > len(prefix) && pos < len(prefix) {
-			// Peek the next chars to see if the message
-			// has the 'error: ' prefix.
-			for nestedPos, nestedChar = range strings.ToLower(prefix) {
-				if unicode.ToLower(r[pos+nestedPos]) != nestedChar {
-					break
-				}
-
-				// Ignore the 'error: ' prefix if it exists.
-				if nestedPos == len(prefix)-1 {
-					pos += len(prefix)
-					// Capitalize the first letter of the message.
-					m = append(m, unicode.ToUpper(r[pos]))
-					pos++
-				}
-			}
+	{
+		// Capitalize the first letter.
+		tmpMessage := []rune(message)
+		if len(tmpMessage) > 0 {
+			tmpMessage[0] = unicode.ToUpper(tmpMessage[0])
 		}
-
-		if len(r)+pos > len(suffix) && pos == len(r)-len(suffix) {
-			// Peek the next chars to see if the message
-			// has the ' error' suffix.
-			for nestedPos, nestedChar = range strings.ToLower(suffix) {
-				if unicode.ToLower(r[pos+nestedPos]) != nestedChar {
-					break
-				}
-
-				// Ignore the ' error' suffix if it exists.
-				// This suffix is usually present in microerrors
-				// without annotations.
-				if nestedPos == len(suffix)-1 {
-					pos += len(suffix)
-				}
-			}
-		}
-
-		// We're already done, let's exit.
-		if pos == len(r) {
-			break
-		}
-
-		char := r[pos]
-		// Capitalize the first letter of the message.
-		if pos == 0 {
-			char = unicode.ToUpper(char)
-		}
-		m = append(m, char)
+		message = string(tmpMessage)
 	}
 
-	return string(m)
+	message = prefix + message
+
+	return message
 }
